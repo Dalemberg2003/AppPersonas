3bd11204e5edcaa5d1fbe2e4d06ee40a59ec8c9f
Dalemberg7 <dalemberismael7@gmail.com>
2022-01-18 20:24:08 -0500

    app personas terminada

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const personas = [new Persona("Marcos", "Martinez"), new Persona("Juan", "Galindo")];
+
+function mostrarPersonas() {
+  let texto = "";
+  for (let persona of personas) {
+    texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+  }
+  document.getElementById("personas").innerHTML = texto;
+}
+
+function agregarPersona() {
+  const forma = document.forms["forma"];
+  const nombre = forma["nombre"];
+  const apellido = forma["apellido"];
+  if (nombre.value != "" && apellido.value != "") {
+    const persona = new Persona(nombre.value, apellido.value);
+    personas.push(persona);
+    mostrarPersonas();
+  }else{
+      alert('No hay informacion que agregar');
+  }
+}
